8c8bc7e34c24f27efa33291c75beb661e934a067
cucmberslice <ywblackberry@gmail.com>
2019-03-20 11:06:59 -0400

    added route path for each component in the BrowserRoute

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-    return (   
-        <div>
-            PageONe
-            <Link to='/pageTwo'>Navigate to page PageTwo</Link>
-        </div>)
-}
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
 
 
-const PageTwo = () => {
-    return <div>
-                PageTwo
-                <button>click Me</button>
-                <Link to='/'>Navigate to page PageOne</Link>
-
-            </div>
-}
 const App = () => {
 
     return (
         <div>
            <BrowserRouter>
-                <Route path="/" exact component={PageOne}/>
-                <Route path='/pageTwo' component={PageTwo} />
+               <div>
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/new" exact component={StreamCreate}/>
+                    <Route path="/streams/edit" exact component={StreamEdit}/>
+                    <Route path="/streams/delete" exact component={StreamDelete}/>
+                    <Route path="/streams/show" exact component={StreamShow}/>
+
+
+
+               </div>
             </BrowserRouter>
         </div>
     )

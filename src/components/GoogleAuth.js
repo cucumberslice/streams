4599feaec2b9520a4d642cd18bbad899db7d5d50
cucmberslice <ywblackberry@gmail.com>
2019-03-20 23:16:54 -0400

    googleauth class can now render auth logged in logged out message

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,18 +1,37 @@
 import React from 'react'
 
 class GoogleAuth extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isSignedIn: null
+        }
+    }
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '581832201132-dfhodmg4v58ig2fp86irolb9m311ibfg.apps.googleusercontent.com',
                 scope: 'email'
+            }).then (() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({
+                    isSignedIn: this.auth.isSignedIn.get()
+                })
             })
         })
     }
+
+    renderAuthButton() {
+        if(this.state.isSignedIn === null) {
+            return <div> I dont know if we are signed in</div>
+        } else if(this.state.isSignedIn){
+            return <div>I am not signed In</div>
+        }
+    }
     render() {
         return (
-            <div>GoogleAuth</div>
+            <div>{this.renderAuthButton()}</div>
         )
     }
 }

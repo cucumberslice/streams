7757828b575eccb6a2c9276dea21eeea1c32e780
cucmberslice <ywblackberry@gmail.com>
2019-03-22 12:33:42 -0400

    created helper functions for sign in sign out button

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -27,21 +27,29 @@ class GoogleAuth extends React.Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
     }
 
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+
+    onSignOut = () => {
+        this.auth.signOut();
+    }
+
     renderAuthButton() {
         if(this.state.isSignedIn === null) {
             return null
         } else if(this.state.isSignedIn){
             return (
-                <div>
-                    <button className="ui red google button">
+                
+                    <button onClick={this.onSignOut} className="ui red google button">
                         <i className="google icon"/>
                         Sign Out
                     </button>
-                </div>
+                
             )
         } else {
             return (
-                    <button className="ui red google button">
+                    <button onClick={this.onSignIn} className="ui red google button">
                         <i className="google icon"/>
                         Sign In with Google
                     </button>

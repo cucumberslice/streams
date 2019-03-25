4b170f7e3598df6e99c35523e1506b05911a5dc1
cucmberslice <ywblackberry@gmail.com>
2019-03-25 18:11:05 -0400

    installed redux-form and created two input fields

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'
+
 import authReducer from './authReducer'
 
 export default combineReducers({
     //dummy data just to get rid of the error messages
-   auth: authReducer
+   auth: authReducer,
+   form: formReducer
 })

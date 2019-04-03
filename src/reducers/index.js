46e354823967e3e120439a6ebc58bb84579d155e
cucmberslice <ywblackberry@gmail.com>
2019-04-02 22:11:52 -0400

    created streamReducer and installed lodash

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'
 
 import authReducer from './authReducer'
+import streamReducer from './streamRedcer'
 
 export default combineReducers({
     //dummy data just to get rid of the error messages
    auth: authReducer,
-   form: formReducer
+   form: formReducer,
+   streams: streamReducer
 })

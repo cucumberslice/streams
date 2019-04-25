4aad1b6d78c4f51ec79a7481eebd3f6dfc68c7d5
cucmberslice <ywblackberry@gmail.com>
2019-04-24 21:52:05 -0400

    hooked up streamEdit

--- src/actions/index.js
+++ src/actions/index.js
@@ -39,8 +39,8 @@ export const createStream = formValues => async (dispatch, getState )=> {
         dispatch({ type: FETCH_STREAMS, payload: response.data })
     }
 
-    export const fetchStream = (id) => async dispatch => {
-        const response = await streams.get(`/stream/${id}`)
+    export const fetchStream = id => async dispatch => {
+        const response = await streams.get(`/streams/${id}`)
 
         dispatch({ type: FETCH_STREAM, payload: response.data })
     }
